1afcfc36ada75222aeb4a37831a7203f86f2c6fa
jonatas-oliveira <jonatas.clementino@inloco.com.br>
2020-02-19 14:10:07 -0300

    chore(mock.go): add missing wasabi client methods to mock client

--- mock.go
+++ mock.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/inloco/go-wasabi/assignments"
+	"github.com/inloco/go-wasabi/experiments"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -18,4 +19,24 @@ type MockedClient struct {
 func (m *MockedClient) GenerateAssignment(ctx context.Context, experimentLabel string, userID string) (*assignments.Assignment, error) {
 	args := m.Called(experimentLabel, userID)
 	return args.Get(0).(*assignments.Assignment), args.Error(1)
+}
+
+func (m *MockedClient) CreateExperiment(ctx context.Context, experiment *experiments.Experiment) (*experiments.Experiment, error) {
+	args := m.Called(experiment)
+	return args.Get(0).(*experiments.Experiment), args.Error(1)
+}
+
+func (m *MockedClient) UpdateExperimentState(ctx context.Context, id string, state experiments.ExperimentState) (*experiments.Experiment, error) {
+	args := m.Called(id, state)
+	return args.Get(0).(*experiments.Experiment), args.Error(1)
+}
+
+func (m *MockedClient) CreateBucket(ctx context.Context, bucket *experiments.Bucket) (*experiments.Bucket, error) {
+	args := m.Called(bucket)
+	return args.Get(0).(*experiments.Bucket), args.Error(1)
+}
+
+func (m *MockedClient) GetExperimentByID(ctx context.Context, experimentID string) (*experiments.Experiment, error) {
+	args := m.Called(experimentID)
+	return args.Get(0).(*experiments.Experiment), args.Error(1)
 }
