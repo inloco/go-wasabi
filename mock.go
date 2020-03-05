ddb4bc3cfd583f467f45ce6d68d4e0b50ac56b73
jonatas-oliveira <jonatas.clementino@inloco.com.br>
2020-03-05 09:57:13 -0300

    test(mock.go): add get experiment buckets method to mock

--- mock.go
+++ mock.go
@@ -40,3 +40,8 @@ func (m *MockedClient) GetExperimentByID(ctx context.Context, experimentID strin
 	args := m.Called(experimentID)
 	return args.Get(0).(*experiments.Experiment), args.Error(1)
 }
+
+func (m *MockedClient) GetExperimentBuckets(ctx context.Context, experimentID string) ([]*experiments.Bucket, error) {
+	args := m.Called(experimentID)
+	return args.Get(0).([]*experiments.Bucket), args.Error(1)
+}
